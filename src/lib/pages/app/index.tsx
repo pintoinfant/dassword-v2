8ccd3e00b3800e4e8772d7f8681c9e3ddd6647bf
pintoinfant <pintoinfant5650@gmail.com>
2023-02-28 07:14:51 +0530

    app page

--- src/lib/pages/app/index.tsx
+++ src/lib/pages/app/index.tsx
@@ -20,7 +20,9 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure
+  useDisclosure,
+  Select
+
 } from '@chakra-ui/react';
 import { ReactElement, useState } from 'react';
 import { useAccount } from "wagmi"
@@ -34,80 +36,39 @@ interface FeatureProps {
 
 export default function Application() {
   const { address } = useAccount()
+  const [credType, setCredType] = useState("")
   const [showPassword, setShowPassword] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    // <Container maxW={'5xl'} py={12}>
-    //   <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-    //     <Stack spacing={4}>
-    //       <Text
-    //         textTransform={'uppercase'}
-    //         color={'blue.400'}
-    //         fontWeight={600}
-    //         fontSize={'sm'}
-    //         bg={useColorModeValue('blue.50', 'blue.900')}
-    //         p={2}
-    //         alignSelf={'flex-start'}
-    //         rounded={'md'}>
-    //         Our Story
-    //       </Text>
-    //       <Heading></Heading>
-    //       <Text color={'gray.500'} fontSize={'lg'}>
-    //         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-    //         nonumy eirmod tempor invidunt ut labore
-    //       </Text>
-    //       <Stack
-    //         spacing={4}
-    //         divider={
-    //           <StackDivider
-    //             borderColor={useColorModeValue('gray.100', 'gray.700')}
-    //           />
-    //         }>
-    //         <Feature
-    //           // icon={
-    //           //   // <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
-    //           // }
-    //           iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-    //           text={'Business Planning'}
-    //         />
-    //         <Feature
-    //           // icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
-    //           iconBg={useColorModeValue('green.100', 'green.900')}
-    //           text={'Financial Planning'}
-    //         />
-    //         <Feature
-    //           // icon={
-    //           //   <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
-    //           // }
-    //           iconBg={useColorModeValue('purple.100', 'purple.900')}
-    //           text={'Market Analysis'}
-    //         />
-    //       </Stack>
-    //     </Stack>
-    //     <Flex>
-    //       <Image
-    //         rounded={'md'}
-    //         alt={'feature image'}
-    //         src={
-    //           'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-    //         }
-    //         objectFit={'cover'}
-    //       />
-    //     </Flex>
-    //   </SimpleGrid>
-    // </Container>
     <Box>
-      <Flex>
-        <Button onClick={onOpen}>Add New Password</Button>
+      <Flex
+        as="header"
+        width="full"
+        align="center"
+        alignSelf="flex-start"
+        justifyContent="center"
+        gridGap={2}
+      >
+        <Box marginLeft="auto">
+          <Button onClick={onOpen}>Add New Password</Button>
+        </Box>
       </Flex>
+      <Box minH={'60vh'}>
+      </Box>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Add New Password</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Stack spacing={4}>
+            <Select placeholder='Select a type' size={'md'} variant={'filled'} onChange={(e) => setCredType(e.target.value)}>
+              <option value='app-pass'>App Password</option>
+              <option value='web-pass'>Web Password</option>
+              <option value='server-cred'>Server Credential</option>
+              <option value='database-cred'>Database Credential</option>
+            </Select>
+            {/* <Stack spacing={4}>
               <HStack>
                 <Box>
                   <FormControl id="firstName" isRequired>
@@ -136,12 +97,11 @@ export default function Application() {
                       onClick={() =>
                         setShowPassword((showPassword) => !showPassword)
                       }>
-                      {/* {showPassword ? <ViewIcon /> : <ViewOffIcon />} */}
                     </Button>
                   </InputRightElement>
                 </InputGroup>
               </FormControl>
-            </Stack>
+            </Stack> */}
           </ModalBody>
           <ModalFooter>
             <Button colorScheme='blue' mr={3} onClick={onClose}>
